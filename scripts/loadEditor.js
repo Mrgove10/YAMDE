89897b9686c95e629a817dde4bbc67994fd068ef
Adrien RICHARD <richardadrien0@gmail.com>
2019-05-24 15:43:24 +0200

    V1, working in teh html format

---
+++ scripts/loadEditor.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const amdLoader = require('../node_modules/monaco-editor/min/vs/loader.js');
+const amdRequire = amdLoader.require;
+const amdDefine = amdLoader.require.define;
+
+function uriFromPath(_path) {
+	var pathName = path.resolve(_path).replace(/\\/g, '/');
+	if (pathName.length > 0 && pathName.charAt(0) !== '/') {
+		pathName = '/' + pathName;
+	}
+	return encodeURI('file://' + pathName);
+}
+amdRequire.config({
+	baseUrl: uriFromPath(path.join(__dirname, '../node_modules/monaco-editor/min'))
+});
+// workaround monaco-css not understanding the environment
+self.module = undefined;
+amdRequire(['vs/editor/editor.main'], function () {
+	var editor = monaco.editor.create(document.getElementById('container'), {
+		//https://microsoft.github.io/monaco-editor/api/interfaces/monaco.editor.ieditorconstructionoptions.html
+		/*value: [
+			'function x() {',
+			'\tconsole.log("Hello world!");',
+			'}'
+		].join('\n'),*/
+		language: 'markdown',
+		automaticLayout:true,
+		lineNumbers: "on",
+		roundedSelection: true,
+		scrollBeyondLastLine: false,
+		readOnly: false,
+		theme: "vs-dark",
+		wordWrap: "bounded"	,
+		wrappingIndent: "same",
+		formatOnPaste :true,
+	});
+	var myBinding = editor.addCommand(monaco.KeyCode.any, function() {
+		console.log('hey');
+	});
+	//this is executed when a ctrl+s
+	editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_S, function() {
+    	console.log('SAVE pressed!');
+	});
+	//This is executed everytime a key is pressed
+	editor.onKeyDown(function() { 
+		document.getElementById('render').innerHTML = editor.getValue()
+		console.log("keyDown"); 
+	}); 	
+});	
